a63b785270dafbae0dc129860d5b2054d95060f4
Anthony Climer <anthonyclimer0925@gmail.com>
2023-01-06 17:04:25 -0500

    All done for now, I am only displaying the names currently

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = props => {
+    return(
+        <div className = 'CharacterWrapper'>
+            {props.data.name}
+
+
+        </div>
+    )
+}
+export default Character;
