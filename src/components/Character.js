b83490996335dfe67298052d204b3ae74d1e67b3
Anthony Climer <anthonyclimer0925@gmail.com>
2023-01-07 15:36:59 -0500

    I have completed the mvp challenges

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,13 +1,36 @@
 // Write your Character component here
 import React from 'react';
+import styled from 'styled-components'
+
+const CharWrap = styled.div `
+    text-align:center;
+    border:1px solid black;
+    border-radius:10px;
+    h3{
+        color:black;
+        border:1px solid black;
+        display:flex;
+        align-items:center;
+        margin-left:20px;
+        &:hover {
+            border:2px solid yellow;
+            scale:1.5;
+        }
+    }
+
+    display:flex;
+    justify-content:space-around;
+    align-items:center;
+`
 
 const Character = props => {
     return(
-        <div className = 'CharacterWrapper'>
-            {props.data.name}
+        <CharWrap className = 'CharacterWrapper'>
+            <h1>Name: {props.data.name}</h1>
+            <h3> - {props.data.birth_year}</h3>
+            <h3> - {props.data.gender}</h3>
 
-
-        </div>
+        </CharWrap>
     )
 }
 export default Character;
